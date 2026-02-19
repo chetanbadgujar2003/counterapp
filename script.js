f0647ee50ec8c5aab7324824e9d498f79d8c3f95
chetanbadgujar2003 <badgujar.chetan585@gmail.com>
2026-02-19 10:22:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function increase() {
+    count++;
+    document.getElementById("count").innerHTML = count;
+}
+
+function decrease() {
+    count--;
+    document.getElementById("count").innerHTML = count;
+}
